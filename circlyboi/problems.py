2eeac15edb41e7a0051971552d83e4056dcaee15
Matthew Bradley <matthewbradley2002@gmail.com>
2026-03-23 02:12:47 -0700

    docs: add original MIT licence back to problems.py

--- circlyboi/problems.py
+++ circlyboi/problems.py
@@ -1,3 +1,30 @@
+# much if the math is taken from the paper:
+#     - https://studenttheses.uu.nl/bitstream/handle/20.500.12932/29861/thesis.pdf?sequence=2
+# and much of the code in this section is taken from (or at the very least inspired by)
+# this repository:
+#     - https://github.com/jeverink/BachelorsThesis/
+# The original MIT license applies to this code
+
+# Copyright (c) 2018 jeverink
+
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import numpy as np
 import numpy.linalg as lin
 
